4a90cd7b81fc062d9fc671cf7e8516203ffcb543
HamSungJun <tjdwns5123@gmail.com>
2024-04-29 22:42:32 +0900

    Setup-03

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,10 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'prettier.config.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'prettier.config.cjs', 'postcss.config.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
@@ -15,5 +17,27 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'import/order': [
+      "error",
+      {
+        "groups": ["builtin","external","internal",["parent", "sibling"],"index","object","type","unknown"],
+        "pathGroups": [
+          {
+            "pattern": "@/**",
+            "group": "unknown"
+          },
+        ],
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ],
   },
+  settings: {
+    "import/resolver": {
+      "typescript": {}
+    }
+  }
 }
